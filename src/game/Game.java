2dabef7ceb40d74646a0f04bae5ec439796b75c3
tiltedlistener <tiltedlistener@gmail.com>
2014-07-30 00:50:23 -0700

    Adding loop interpolation, render check, and update check

--- src/game/Game.java
+++ src/game/Game.java
@@ -8,28 +8,34 @@ import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 import javax.swing.JFrame;
 
-public class Game extends JFrame implements Runnable, KeyListener{
+abstract class Game extends JFrame implements Runnable, KeyListener{
 	
 	private static final long serialVersionUID = 1;
 	
 	// Game loop
 	private Thread gameLoop;
-	private int desiredFrameRate;
+	private float interpolation;
+	private int fps = 60;
+	private int frameCount = 0;
 	
 	// Graphics rendering
 	private BufferedImage backBuffer;
 	private Graphics2D g2d;
 	private int screenWidth, screenHeight;
 	
+	// Game State
+	boolean paused = false;
 	
-	// Key States
-	boolean keyDown, keyUp, keyLeft, keyRight, keyFire;	
+	// Absract Methods
+	abstract void gamePaint(float interpolation);
+	abstract void gameUpdate();
+	abstract void gameKeyDown(int keyCode);
+	abstract void gameKeyUp(int keyCode);
 	
 	public Game(String name, int width, int height) {
 		super(name);
 		this.screenWidth = width;
 		this.screenHeight = height;
-		this.desiredFrameRate = 60;
 		
 		// Graphics Setup
 		backBuffer = new BufferedImage(screenWidth, screenHeight, BufferedImage.TYPE_INT_RGB);
@@ -58,26 +64,113 @@ public class Game extends JFrame implements Runnable, KeyListener{
 	
 	public void run() {
 		Thread t = Thread.currentThread();
+		
+		/**
+		 * Credit to Eli Delvanthal for the loop structure
+		 * http://www.java-gaming.org/index.php?topic=24220.0
+		 */
+		final double GAME_HERTZ = 30.0;
+		
+		// Calculate how many ns each frame should take for our target game hertz.
+		final double TIME_BETWEEN_UPDATES = 1000000000 / GAME_HERTZ;
+		
+		// At the very most we will update the game this many times before a new render.
+		final int MAX_UPDATES_BEFORE_RENDER = 5;
+		
+		//We will need the last update time.
+		double lastUpdateTime = System.nanoTime();
+		
+		// Store the last time we rendered.
+		double lastRenderTime = System.nanoTime();
+		  
+		// If we are able to get as high as this FPS, don't render again.
+		final double TARGET_FPS = 60;
+		final double TARGET_TIME_BETWEEN_RENDERS = 1000000000 / TARGET_FPS;
+		
+		//Simple way of finding FPS.
+	    int lastSecondTime = (int) (lastUpdateTime / 1000000000);
+		
 		while(t == gameLoop) {
-			try {
-				Thread.sleep(1000/desiredFrameRate);
-			} catch(InterruptedException e) {
-				e.printStackTrace();
-			}
+			
+			double now = System.nanoTime();
+			int updateCount = 0;
+			 
+			if (!paused) {
+				//Do as many game updates as we need to, potentially playing catchup.
+				while( now - lastUpdateTime > TIME_BETWEEN_UPDATES && updateCount < MAX_UPDATES_BEFORE_RENDER ) {
+					gameUpdate();
+					lastUpdateTime += TIME_BETWEEN_UPDATES;
+					updateCount++;
+				}
 
-			repaint();
+				//If for some reason an update takes forever, we don't want to do an insane number of catchups.
+				//If you were doing some sort of game that needed to keep EXACT time, you would get rid of this.
+				if ( now - lastUpdateTime > TIME_BETWEEN_UPDATES) {
+				   lastUpdateTime = now - TIME_BETWEEN_UPDATES;
+				}
+
+				//Render. To do so, we need to calculate interpolation for a smooth render.
+				float interpolation = Math.min(1.0f, (float) ((now - lastUpdateTime) / TIME_BETWEEN_UPDATES) );
+				render(interpolation);
+				lastRenderTime = now;
+
+				//Update the frames we got.
+				int thisSecond = (int) (lastUpdateTime / 1000000000);
+				if (thisSecond > lastSecondTime) {
+				   fps = frameCount;
+				   frameCount = 0;
+				   lastSecondTime = thisSecond;
+				}
+
+				//Yield until it has been at least the target time between renders. This saves the CPU from hogging.
+				while ( now - lastRenderTime < TARGET_TIME_BETWEEN_RENDERS && now - lastUpdateTime < TIME_BETWEEN_UPDATES) {
+				   Thread.yield();
+
+				   //This stops the app from consuming all your CPU. It makes this slightly less accurate, but is worth it.
+				   //You can remove this line and it will still work (better), your CPU just climbs on certain OSes.
+				   //FYI on some OS's this can cause pretty bad stuttering. Scroll down and have a look at different peoples' solutions to this.
+				   try {Thread.sleep(1);} catch(Exception e) {} 
+
+				   now = System.nanoTime();
+				} 
+			
+			}			
 		}
 	}
+	
+	public void setInterpolation(float interp) {
+       interpolation = interp;
+    } 
 	
 	public void stop() {
 		gameLoop = null;
 	}
 	
 	/**
+	 * GAME STATE
+	 */
+	public void pauseGame() {
+		paused = true;
+	}
+	
+	public void unpauseGame() {
+		paused = false;
+	}
+	
+	/**
 	 * RENDER
 	 */
+	public void render(float interpolation) {
+		setInterpolation(interpolation);
+		repaint();
+	}
+	
 	public void paint(Graphics g) {
-		g2d.clearRect(0, 0, this.screenWidth, this.screenHeight);
+		g2d.clearRect(0, 0, this.screenWidth, this.screenHeight);		
+		
+		// Draw individual components
+		gamePaint(interpolation);
+		
 		g.drawImage(backBuffer, 0, 0, this);
 	}	
 	
@@ -86,36 +179,10 @@ public class Game extends JFrame implements Runnable, KeyListener{
 	 */
 	public void keyTyped(KeyEvent k) {}
 	public void keyPressed(KeyEvent k) {
-		switch(k.getKeyCode()) {
-		case KeyEvent.VK_LEFT:
-			keyLeft = true;
-			break;
-		case KeyEvent.VK_RIGHT:
-			keyRight = true;
-			break;
-		case KeyEvent.VK_UP:
-			keyUp = true;
-			break;	
-		case KeyEvent.VK_SPACE:
-			keyFire = true;
-			break;
-		}
+		gameKeyDown(k.getKeyCode());
 	}
 	public void keyReleased(KeyEvent k) {
-		switch(k.getKeyCode()) {
-		case KeyEvent.VK_LEFT:
-			keyLeft = false;
-			break;
-		case KeyEvent.VK_RIGHT:
-			keyRight = false;
-			break;
-		case KeyEvent.VK_UP:
-			keyUp = false;
-			break;	
-		case KeyEvent.VK_SPACE:
-			keyFire = false;
-			break;
-		}
+		gameKeyUp(k.getKeyCode());
 	}
 	
 }
--- src/game/GhostHunter.java
+++ src/game/GhostHunter.java
@@ -1,11 +1,57 @@
 package game;
 
+import java.awt.event.KeyEvent;
+
 public class GhostHunter extends Game {
 
 	private static final long serialVersionUID = 2;
 	
+	// Key States
+	boolean keyDown, keyUp, keyLeft, keyRight, keyFire;	
+	
 	public GhostHunter() {
 		super("Ghost Hunter", 1024, 1024);
+	}
+	
+	public void gamePaint(float interpolation) {
+		System.out.println(interpolation);
+	}
+	
+	public void gameUpdate() {
+		
+	}
+	
+	public void gameKeyDown(int k) {
+		switch(k) {
+		case KeyEvent.VK_LEFT:
+			keyLeft = true;
+			break;
+		case KeyEvent.VK_RIGHT:
+			keyRight = true;
+			break;
+		case KeyEvent.VK_UP:
+			keyUp = true;
+			break;	
+		case KeyEvent.VK_SPACE:
+			keyFire = true;
+			break;
+		}
+	}
+	public void gameKeyUp(int k) {
+		switch(k) {
+		case KeyEvent.VK_LEFT:
+			keyLeft = false;
+			break;
+		case KeyEvent.VK_RIGHT:
+			keyRight = false;
+			break;
+		case KeyEvent.VK_UP:
+			keyUp = false;
+			break;	
+		case KeyEvent.VK_SPACE:
+			keyFire = false;
+			break;
+		}
 	}
 	
 }
